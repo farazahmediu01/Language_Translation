dca8e9332c256c190bfc8b3637ba96c763b01338
Faraz Ahmed <farazahmediu01@gmail.com>
2022-02-03 17:32:17 +0500

    every thing is working fine but consumming lots of time\]

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
-from deep_translator import GoogleTranslator, single_detection
-from keys import api_key
-import requests
+from fnmatch import translate
+from deep_translator import GoogleTranslator
 from bs4 import BeautifulSoup
+from keys import api_key
+import pandas as pd
 import people_also_ask
+import requests
+import uuid
 
-def status_paa(response, ppa: list):
+
+# variables
+text = "water"
+languages = GoogleTranslator.get_supported_languages()
+ppa = list()  # people_also_ask
+translations = list()
+
+# contain a list included all translations
+for language in languages:
+    result = GoogleTranslator(source='auto', target=language).translate(
+        text=text)  # video only include text not text=text
+    translations.append(result)
+
+for text in translations:
+    response = people_also_ask.get_related_questions(text)
     if response:
         ppa.append(True)
     else:
         ppa.append(False)
 
 
-# variables
-text = "never give up"
-languages =  GoogleTranslator.get_supported_languages()[:5] + ["urdu", "english"]# only 5 languages
-translations = list()
-ppa = list() # people_also_ask
-
-
-# contain a list included all translations
-for language in languages:
-    result = GoogleTranslator(source='auto', target=language).translate(text=text) # video only include text not text=text
-    translations.append(result)
-
-for text in translations:
-    response = people_also_ask.get_related_questions(text)
-    status_paa(response, ppa)
-
-print(translations)
-print()
-print(ppa)
-
-
-
-
-
-
-
-
-
-
-
-# print(translations)
+all_variables = {"String": translations, "Translated_Language": [
+    f"{v}_{k}" for k, v in languages.items()], "PPA": ppa}
+df = pd.DataFrame(all_variables)
+df.to_csv(str(uuid.uuid4()) + '.csv')
