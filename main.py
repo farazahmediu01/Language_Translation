1f7500c0b73e0fb2371da13f7dbd4153f800f17b
Faraz Ahmed <farazahmediu01@gmail.com>
2022-02-03 16:05:30 +0500

    main file working fine

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-from enum import auto
-from fnmatch import translate
-from unittest import result
+# from enum import auto
+# from fnmatch import translate
+# from unittest import result
 from deep_translator import GoogleTranslator, single_detection
 from keys import api_key
 import requests
 from bs4 import BeautifulSoup
 
 
-text = "water"
-languages =  GoogleTranslator.get_supported_languages()[:5] # only 5 languages
-# print(lang_dict)
-# print(len(lang_dict))
-translation = list()
+text = "never give up"
+languages =  GoogleTranslator.get_supported_languages()[:5] + ["urdu"]# only 5 languages
+translations = list()
+paa = list()
 for language in languages:
     result = GoogleTranslator(source='auto', target=language).translate(text=text) # video only include text not text=text
-    translation.append(result)
+    translations.append(result)
 
-print(translation)
 
-# print(transalte)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(translations)
